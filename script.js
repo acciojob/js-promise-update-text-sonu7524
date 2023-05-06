9cdc69332727bfe9abe45097c6e5dfac743a59f7
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-05-06 11:41:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function sayHello() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+const outputElement = document.getElementById("output");
+
+sayHello().then(message => {
+  outputElement.textContent = message;
+});
